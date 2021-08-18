67dff9eb318b6994a23543fe82d4f6c0aa87a362
nick <butnick63@gmail.com>
2021-08-19 01:40:53 +0300

    fix

--- read_data.py
+++ read_data.py
@@ -122,6 +122,7 @@ def read2_tracks_csv(track_csv_path, tracks_meta, track_meta_recording):
             LONACCELERATION: rows[LONACCELERATION].values,
             LATACCELERATION: rows[LATACCELERATION].values,
 
+
         }
         current_track = current_track + 1
     return tracks
