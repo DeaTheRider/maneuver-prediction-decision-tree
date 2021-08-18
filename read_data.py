0d7eab48ae3d2ca3201a014638d11adc82938486
DeaTheRider <lol131411@gmail.com>
2021-08-19 01:43:29 +0300

    ads

--- read_data.py
+++ read_data.py
@@ -123,6 +123,7 @@ def read2_tracks_csv(track_csv_path, tracks_meta, track_meta_recording):
             LATACCELERATION: rows[LATACCELERATION].values,
 
 
+
         }
         current_track = current_track + 1
     return tracks
