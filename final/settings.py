24356d101a9927949b627c3010611a0e995780a1
tamat <uefv8ctxGH>
2021-08-25 22:42:03 +0300

    final

---
+++ final/settings.py
@@ -0,0 +1,11 @@
+INITIAL_DATASET_FOLDER = 'datasets/'
+CONCATENATED_DATASET_FOLDER =  'prepared_dataset/'
+
+DATASETS = {
+    'rounD-dataset-v1.0':{
+        'initial_csv_count': 24,
+    },
+    'inD-dataset-v1.0':{
+        'initial_csv_count': 33,
+    }
+}
