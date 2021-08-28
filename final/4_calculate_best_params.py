3616cee5efba257dc8da96b2a73c1836d6eb05ba
tamat <uefv8ctxGH>
2021-08-28 22:46:27 +0300

    final/4

--- final/4_calculate_best_params.py
+++ final/4_calculate_best_params.py
@@ -10,14 +10,14 @@ import settings
 SEARCH_PARAMS = {
     'criterion': ['gini', 'entropy'],
     'max_depth': range(1, 10),
-    'min_samples_split': range(1, 10),
+    'min_samples_split': range(2, 10),
     'min_samples_leaf': range(1, 5)
 }
 
 
 def run_once(dataset, filepath):
     class_name = filepath.name[:-4]
-    print(f'Starting {class_name}')
+    print(f'Starting {dataset["dataset_name"]} {class_name}')
     df = pd.read_csv(filepath)
     df = df.drop(columns=['recordingId', 'frame', 'trackId', 'class'])
     print(df['prediction'].value_counts())
@@ -33,27 +33,13 @@ def run_once(dataset, filepath):
     clf.fit(x_train, y_train)
     with open(f'{settings.BEST_PARAMETERS_FOLDER}/{dataset["dataset_name"]}/{class_name}/parameters.json', 'w') as f:
         json.dump(clf.best_params_, f)
-    print(f'Finished {class_name}')
+    print(f'Finished {dataset["dataset_name"]} {class_name}')
 
 
 def run_all():
     for name, dataset in settings.DATASETS.items():
-        print(f'Starting {dataset["dataset_name"]}')
         for filepath in Path(f'{settings.LABELED_DATASET_FOLDER}/{dataset["dataset_name"]}/').glob('*.csv'):
             run_once(dataset, filepath)
-
-
-def run_all_multiprocessing():
-    all_processes = []
-    for name, dataset in settings.DATASETS.items():
-        print(f'Starting {dataset["dataset_name"]}')
-        for filepath in Path(f'{settings.LABELED_DATASET_FOLDER}/{dataset["dataset_name"]}/').glob('*.csv'):
-            p = Process(target=run_once, args=(dataset, filepath))
-            p.start()
-            p.join()
-
-    for p in all_processes:
-        p.join()
 
 
 run_all()
